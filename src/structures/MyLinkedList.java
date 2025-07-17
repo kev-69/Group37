c6be1ad36eb441e927760271e377107e1a896677
kev-69 <bismarkobuobi19@gmail.com>
2025-07-17 10:30:51 +0000

    Refactor MyLinkedList class to support generics and enhance functionality with additional methods for better data management

--- src/structures/MyLinkedList.java
+++ src/structures/MyLinkedList.java
@@ -1,4 +1,200 @@
 package structures;
 
-public class MyLinkedList {
+/**
+ * Custom LinkedList implementation for storing generic objects
+ * 
+ * @param <T> the type of elements stored in this list
+ */
+public class MyLinkedList<T> {
+    private Node<T> head;
+    private Node<T> tail;
+    private int size;
+
+    /**
+     * Node class for the linked list
+     */
+    private static class Node<T> {
+        T data;
+        Node<T> next;
+        Node<T> prev;
+
+        Node(T data) {
+            this.data = data;
+            this.next = null;
+            this.prev = null;
+        }
+    }
+
+    public MyLinkedList() {
+        this.head = null;
+        this.tail = null;
+        this.size = 0;
+    }
+
+    // Basic operations
+    public void add(T item) {
+        Node<T> newNode = new Node<>(item);
+
+        if (head == null) {
+            head = tail = newNode;
+        } else {
+            tail.next = newNode;
+            newNode.prev = tail;
+            tail = newNode;
+        }
+        size++;
+    }
+
+    public void addFirst(T item) {
+        Node<T> newNode = new Node<>(item);
+
+        if (head == null) {
+            head = tail = newNode;
+        } else {
+            newNode.next = head;
+            head.prev = newNode;
+            head = newNode;
+        }
+        size++;
+    }
+
+    public T remove(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+
+        Node<T> nodeToRemove = getNode(index);
+        return removeNode(nodeToRemove);
+    }
+
+    public boolean remove(T item) {
+        Node<T> current = head;
+
+        while (current != null) {
+            if ((current.data == null && item == null) ||
+                    (current.data != null && current.data.equals(item))) {
+                removeNode(current);
+                return true;
+            }
+            current = current.next;
+        }
+        return false;
+    }
+
+    private T removeNode(Node<T> node) {
+        if (node.prev != null) {
+            node.prev.next = node.next;
+        } else {
+            head = node.next;
+        }
+
+        if (node.next != null) {
+            node.next.prev = node.prev;
+        } else {
+            tail = node.prev;
+        }
+
+        size--;
+        return node.data;
+    }
+
+    public T get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+        return getNode(index).data;
+    }
+
+    private Node<T> getNode(int index) {
+        Node<T> current;
+
+        if (index < size / 2) {
+            current = head;
+            for (int i = 0; i < index; i++) {
+                current = current.next;
+            }
+        } else {
+            current = tail;
+            for (int i = size - 1; i > index; i--) {
+                current = current.prev;
+            }
+        }
+        return current;
+    }
+
+    public boolean contains(T item) {
+        Node<T> current = head;
+
+        while (current != null) {
+            if ((current.data == null && item == null) ||
+                    (current.data != null && current.data.equals(item))) {
+                return true;
+            }
+            current = current.next;
+        }
+        return false;
+    }
+
+    public int indexOf(T item) {
+        Node<T> current = head;
+        int index = 0;
+
+        while (current != null) {
+            if ((current.data == null && item == null) ||
+                    (current.data != null && current.data.equals(item))) {
+                return index;
+            }
+            current = current.next;
+            index++;
+        }
+        return -1;
+    }
+
+    public void clear() {
+        head = null;
+        tail = null;
+        size = 0;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public T[] toArray() {
+        @SuppressWarnings("unchecked")
+        T[] array = (T[]) new Object[size];
+        Node<T> current = head;
+        int index = 0;
+
+        while (current != null) {
+            array[index++] = current.data;
+            current = current.next;
+        }
+        return array;
+    }
+
+    @Override
+    public String toString() {
+        if (isEmpty()) {
+            return "[]";
+        }
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        Node<T> current = head;
+
+        while (current != null) {
+            sb.append(current.data);
+            if (current.next != null) {
+                sb.append(", ");
+            }
+            current = current.next;
+        }
+        sb.append("]");
+        return sb.toString();
+    }
 }
